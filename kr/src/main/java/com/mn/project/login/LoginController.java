102317e8709d05e6be5c9f4750dac489af140dfb
11 <11@11-PC>
2020-01-20 17:31:09 +0900

    0120

--- kr/src/main/java/com/mn/project/login/LoginController.java
+++ kr/src/main/java/com/mn/project/login/LoginController.java
@@ -200,7 +200,7 @@ public class LoginController {
          }else {
         	  
         	  service.mmdelete(loginVO);
-        	 return "redirect:/"; 
+        	 return "/First/deletecomplete";
          }
 	}	
 }
